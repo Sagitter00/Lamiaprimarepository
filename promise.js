2d6b676b0e2981af6903a6c7d1b0af896f67c1a2
Federico Palloni <federpallon.federpallon@gmail.com>
2023-05-15 16:38:26 +0200

    esercizio

--- promise.js
+++ promise.js
@@ -1,54 +1,53 @@
-function primaOperazione(input, genoveffa) {
-    const output = ' primaOperazione ' + input;
-    console.log(output);
-    if(genoveffa) {
-      genoveffa(output);
-    }
+function primaOperazione(input, callback) {
+    setTimeout(() => {
+      const output = ' primaOperazione ' + input;
+      console.log(output);
+      if (callback) {
+        callback(output);
+      }
+    }, 0)
   }
   
-  function secondaOperazione(input, genoveffa) {
-    const output = ' secondaOperazione ' + input;
-    console.log(output);
-    if(genoveffa) {
-      genoveffa(output);
+  function secondaOperazione(input, callback) {
+    setTimeout(() => {
+      const output = ' secondaOperazione ' + input;
+      console.log(output);
+      if (callback) {
+        callback(output);
+      }
+    }, 0)
+  }
+
+function primaOperazionePromise(input){
+    const promessa = new Promise((resolve,reject) =>{
+        primaOperazione(input, (errore, risultato) =>{
+            if(errore){
+                reject(errore)
+            }else{
+                resolve(risultato);
+            }
+        })
+    },)
+    return promessa;
+}
+function secondaOperazionePromise(input){
+    const promessa = new Promise((resolve,reject) =>{
+        secondaOperazione(input, (errore, risultato) =>{
+            if(errore){
+                reject(errore)
+            }else{
+                resolve(risultato);
+            }
+        })
+    },)
+    return promessa;
+}
+
+const save = primaOperazionePromise(60).then((risultato) =>{
+    return secondaOperazionePromise(risultato)
     }
-  }
-  function primaOperazionePromise(input){
-    const p = new Promise(
-        (resolve,reject) =>{
-            primaOperazione(input, (output1) =>{
-                resolve(output1)
-            })
-        }
+    ).then(
+        (risultato2)=>{
+        console.log("Fatto!")
+    }
     )
-    return p;
-  }
-
-  function secondaOperazionePromise(input){
-    const p = new Promise(
-        (resolve,reject) =>{
-            secondaOperazione(input, (output2) =>{
-                resolve(output2)
-            })
-        }
-    )
-    return p;
-  }
-
-const genoveffa1 = primaOperazionePromise("esperimento")
-    .then(
-        (output1) =>{
-            return secondaOperazionePromise(output1)
-        }
-    )
-const genoveffa2 = genoveffa1
-    .then(
-        (output2) =>{
-            console.log(output2)
-            return output2;
-        }
-    );
-  
-const fine = genoveffa2.then(
-    finale => console.log(finale)
-)
