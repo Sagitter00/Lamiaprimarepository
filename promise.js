6120dbd896c92541e995a1ad1c1a320af3171324
Federico Palloni <federpallon.federpallon@gmail.com>
2023-05-15 15:39:45 +0200

    promises

---
+++ promise.js
@@ -0,0 +1,50 @@
+function primaOperazione(input, genoveffa) {
+    const output = ' primaOperazione ' + input;
+    console.log(output);
+    if(genoveffa) {
+      genoveffa(output);
+    }
+  }
+  
+  function secondaOperazione(input, genoveffa) {
+    const output = ' secondaOperazione ' + input;
+    console.log(output);
+    if(genoveffa) {
+      genoveffa(output);
+    }
+  }
+  function primaOperazionePromise(input){
+    const p = new Promise(
+        (resolve,reject) =>{
+            primaOperazione(input, resolve)
+        }
+    )
+    return p;
+  }
+
+  function secondaOperazionePromise(input){
+    const p = new Promise(
+        (resolve,reject) =>{
+            secondaOperazione(input, resolve)
+        }
+    )
+    return p;
+  }
+
+const genoveffa1 = primaOperazionePromise("esperimento")
+    .then(
+        (output1) =>{
+            return secondaOperazionePromise(output1)
+        }
+    )
+const genoveffa2 = genoveffa1
+    .then(
+        (output2) =>{
+            console.log(output2)
+            return output2;
+        }
+    );
+  
+const fine = genoveffa2.then(
+    finale => console.log(finale)
+)
